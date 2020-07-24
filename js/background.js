82247dfd77785fcfdb9878126ab18bfa7161c38d
eggplantcx <tsnootson@gmail.com>
2020-07-24 18:11:46 -0500

    add code

---
+++ js/background.js
@@ -0,0 +1,13 @@
+chrome.runtime.onInstalled.addListener(()=>{
+    chrome.storage.local.set({enabled:true});
+});
+let e;
+chrome.webRequest.onBeforeRequest.addListener((d)=>{
+    chrome.storage.local.get(["enabled"], (i)=>e = i.enabled);
+    if(e == true){
+        console.log("redirected " + d.url);
+        return{redirectUrl: d.url.replace("www", "smile")};
+    }
+}, 
+{urls:["*://www.amazon.com/*"]}, 
+["blocking"]);
